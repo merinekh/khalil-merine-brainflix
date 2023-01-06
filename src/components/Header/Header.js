55c5c871353adf13fb55b98eef47bdb227f05283
Khalil Merine <merinekh@gmail.com>
2023-01-06 00:38:04 -0500

    Main components empty + Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import "./Header.scss";
+import { IoLogoYoutube } from "react-icons/io";
+import BrainflixLogo from "../../assets/images/BrainFlix-logo.svg";
+import Avatar from "../../assets/images/Mohan-muruge.jpg";
+import upload from "../../assets/images/upload.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <img src={BrainflixLogo} alt="" className="header-logo__logo" />
+      </div>
+      <div className="header-search">
+        <input
+          className="header-search__input"
+          type="search"
+          name="search"
+          id=""
+        />
+        <img src={Avatar} alt="" className="header-search__avatar" />
+      </div>
+      <button className="header-button">
+        <img src={upload} alt="" className="header-button__icon" />
+        <h3 className="header-button__text">UPLOAD</h3>
+      </button>
+    </header>
+  );
+}
+
+export default Header;
