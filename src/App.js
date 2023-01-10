d3e14e3e88ef5243ac23b6ef75892f7a2e21a86c
Khalil Merine <merinekh@gmail.com>
2023-01-09 21:30:40 -0500

    App.js Clean-up

--- src/App.js
+++ src/App.js
@@ -4,17 +4,9 @@ import Component from "./components/Component/Component";
 import data from "./data/video-details.json";
 
 function App() {
-  // const [videoData, setVideoData] = useState(data[0]);
-  // const choseVid = (index) => {
-  //   setVideoData(data[index]);
-  //   console.log(data[index]);
-  // };
-  // console.log(videoData);
   return (
     <div className="App">
       <Component />
-
-      <div className="desktoplayout"></div>
     </div>
   );
 }
