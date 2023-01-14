074d876d3d0268e94668fb7e37e40ed1d5f858a0
Khalil Merine <merinekh@gmail.com>
2023-01-14 17:23:57 -0500

    Add Route to VideoId

--- src/components/Component/Component.js
+++ src/components/Component/Component.js
@@ -14,6 +14,7 @@ function Component() {
         <Routes>
           <Route path="" element={<HomePage />} />
           <Route path="/videoUpload" element={<Page image={imageUpload} />} />
+          <Route path="/:videoId" element={<Page image={imageUpload} />} />
         </Routes>
       </BrowserRouter>
     </>
