90b996184972baffcd3c5ed02a11268572a5aed3
Khalil Merine <merinekh@gmail.com>
2023-01-15 18:45:20 -0500

    Importing Manualy Code From the Routing Branch

--- src/components/Component/HomePage/HomePage.js
+++ src/components/Component/HomePage/HomePage.js
@@ -1,18 +1,50 @@
-import React from "react";
 import Main from "../Main/Main";
 import Next from "../Next/Next";
 import data from "../../../data/video-details.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Comments from "../Comments/Comments";
+import axios from "axios";
+import { useParams } from "react-router";
 
 function HomePage() {
-  const [videoData, setVideoData] = useState(data[0]);
-  const choseVid = (index) => {
-    setVideoData(index);
-  };
-  // console.log(videoData);
+  // =============Import Video info for SideBar=================
+  const [dataimport, setDataImport] = useState([]);
+  const API_URL = "https://project-2-api.herokuapp.com/";
+  const API_PATH = "videos/";
+  const API_KEY = "?api_key=c473bb50-76c8-4bb6-96a8-4e8e5acefe10";
 
-  let { image } = videoData;
+  useEffect(() => {
+    axios
+      .get(API_URL + API_PATH + API_KEY)
+      .then((response) => {
+        setDataImport(response.data);
+        // console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  // ================Choose Video to Display==============
+  const [videoData, setVideoData] = useState([]);
+  const { Id } = useParams();
+  // console.log(Id);
+  const videoId = Id || "84e96018-4022-434e-80bf-000ce4cd12b8";
+
+  useEffect(() => {
+    // console.log(API_URL + API_PATH + `${videoId}` + API_KEY);
+    if (videoId) {
+      axios
+        .get(API_URL + API_PATH + `${videoId}` + API_KEY)
+        .then((response) => {
+          setVideoData(response.data);
+          // console.log(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [videoId]);
 
   //   console.log(videoData);
   return (
@@ -20,7 +52,7 @@ function HomePage() {
       <Main videoData={videoData} />
       <div className="desktoplayout">
         <Comments videoData={videoData} />
-        <Next videoData={videoData} data={data} choseVid={choseVid} />
+        <Next videoData={videoData} data={dataimport} />
       </div>
     </>
   );
