36dec533317184f43cb122296821d9a2ea384650
Khalil Merine <merinekh@gmail.com>
2023-01-15 18:16:19 -0500

    Upload Page Responsiveness Done

--- src/pages/Page/Page.js
+++ src/pages/Page/Page.js
@@ -16,37 +16,41 @@ function Page({ image }) {
   };
   return (
     <>
+      <hr className="upload-line__header" />
       <div className="upload">
         <h1 className="upload__title">Upload Video</h1>
         <div className="upload-content">
-          <div className="upload-content-video">
-            <p className="upload-content-video__title subtitle">
-              VIDEO THUMBNAIL
-            </p>
-            <img src={image} alt="" className="upload-content-video__video" />
+          <div className="upload-desktoplayer">
+            <div className="upload-content-video">
+              <p className="upload-content-video__title subtitle">
+                VIDEO THUMBNAIL
+              </p>
+              <img src={image} alt="" className="upload-content-video__video" />
+            </div>
+            <form className="upload-content-inputs">
+              <p className="upload-content-inputs__title subtitle">
+                TITLE OF YOUR VIDEO
+              </p>
+              <input
+                type="text"
+                name="newTitle"
+                id="newTitle"
+                className="upload-content-inputs__titleinput"
+                placeholder="Add a title to your video"
+              />
+              <p className="upload-content-inputs__title subtitle">
+                ADD A VIDEO DESCRIPTION
+              </p>
+              <textarea
+                type="text"
+                name="newDescr"
+                id="newDescr"
+                className="upload-content-inputs__titleinput"
+                placeholder="Add a description to your video"
+              />
+            </form>
           </div>
-          <form className="upload-content-inputs">
-            <p className="upload-content-inputs__title subtitle">
-              TITLE OF YOUR VIDEO
-            </p>
-            <input
-              type="text"
-              name="newTitle"
-              id="newTitle"
-              className="upload-content-inputs__titleinput"
-              placeholder="Add a title to your video"
-            />
-            <p className="upload-content-inputs__title subtitle">
-              ADD A VIDEO DESCRIPTION
-            </p>
-            <textarea
-              type="text"
-              name="newDescr"
-              id="newDescr"
-              className="upload-content-inputs__titleinput"
-              placeholder="Add a description to your video"
-            />
-          </form>
+
           <div className="upload-content-buttons">
             <NavLink to="/">
               <button className="upload-content-buttons__cancel">CANCEL</button>
